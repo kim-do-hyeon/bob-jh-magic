0aec22230eb2af3d511eb4562e39b4910c244f0c
pental <pental@kakao.com>
2024-08-04 23:49:42 +0900

    Add sha-1, sha-256 & input name field

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import hashlib
 
 app = Flask(__name__)
 
-def calculate_md5(file):
-    hash_md5 = hashlib.md5()
-    for chunk in iter(lambda: file.read(4096), b""):
-        hash_md5.update(chunk)
+def calculate_hashes(file):
+    file.seek(0)
+    data = file.read()
+    
+    md5_hash = hashlib.md5(data).hexdigest()
+    sha1_hash = hashlib.sha1(data).hexdigest()
+    sha256_hash = hashlib.sha256(data).hexdigest()
+    
     file.seek(0)  # Reset file pointer to the beginning
-    return hash_md5.hexdigest()
+    
+    return md5_hash, sha1_hash, sha256_hash
 
 @app.route('/')
 def index():
@@ -23,8 +28,9 @@ def upload():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
     
-    file_hash = calculate_md5(file)
-    return jsonify({'hash': file_hash, 'message': 'File uploaded and hash calculated'})
+    md5_hash, sha1_hash, sha256_hash = calculate_hashes(file)
+    filename = file.filename
+    return jsonify({'filename': filename, 'md5': md5_hash, 'sha1': sha1_hash, 'sha256': sha256_hash})
 
 if __name__ == '__main__':
     app.run(debug=True)
